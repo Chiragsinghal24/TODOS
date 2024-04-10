66c49879f17946d6d3294e10b1bbac5f0a7333ea
chiragsinghal96 <chiragsinghal2406@gmail.com>
2024-04-10 23:40:45 +0530

    authentication

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.alltodos,name ='alltodos'),
+    path('todo/',views.alltodos,name ='alltodos'),
+    path('',views.Signuppage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
     path('delete_item/<int:pk>',views.deleteItem,name = 'deleteItem'),
-    path('update_item<int:pk>',views.updateItem,name = 'updateItem')
+    path('update_item<int:pk>',views.updateItem,name = 'updateItem'),
+    path('logout/',views.Logoutpage,name='logout'),
 ]
